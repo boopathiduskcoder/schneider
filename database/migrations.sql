4f3aa5baaea5c0f50e9e02270f22d696a83daddb
Boopathi <boopathi.duskcoder@gmail.com>
2022-10-05 21:24:28 +0530

    sample

---
+++ database/migrations.sql
@@ -0,0 +1,11 @@
+-- create table for addition
+
+CREATE TABLE `addition` (
+  `addi_id` int(14) NOT NULL,
+  `salary_id` int(14) NOT NULL,
+  `basic` varchar(128) DEFAULT NULL,
+  `medical` varchar(64) DEFAULT NULL,
+  `house_rent` varchar(64) DEFAULT NULL,
+  `conveyance` varchar(64) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- ----------------------------------------------------
